cef9c6bd1067a6ce599fb751a40b8bc71b1f170b
oustan10 <oustan10@gmail.com>
2019-10-31 03:18:51 -0400

    bust a nut

--- server/app.py
+++ server/app.py
@@ -1,13 +1,22 @@
 import os, datetime, threading
 from flask import *
 from threading import Timer
-from triangulate import triangulate
+from triangulate import *
+from datetime import datetime
+
+# Sound sample
+class Sound:
+	def __init__(self, vol = 0, direc = 0, time = 0):
+		self.vol = vol
+		self.dir = direc
+		self.time = time
 
 # Constants
 UPLOAD_FOLDER = 'uploads'
 x = 3 # size of grid: width
 y = 3 # size of grid: height
-grid = [[1, 2, 3], [3, 2, 3], [2, 3, 1]]
+# grid = [[1, 2, 3], [3, 2, 3], [2, 3, 1]]
+grid = [Sound(13, 50, 3), Sound(14, 12, 23), Sound(150, 24, 58)]
 tdelta = 5.0 # refresh delay
 # for i in range(y):
 # 	grid.append([])
@@ -16,22 +25,20 @@ tdelta = 5.0 # refresh delay
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-# Sound sample
-class Sound:
-	def __init__(vol = 0, direc = 0, time = 0):
-		self.vol = vol
-		self.dir = direc
-		self.time = time
-
 # Info at one coordinate
 # class Square:
-# 	def __init__(level = 0):
+# 	def __init__(self, level = 0):
 # 		self.level = level
 
 # Run a function periodically
 def refresh(f, delay):
 	t = Timer(delay, f)
 	t.start()
+
+def update():
+	global grid
+	grid = gen_random(datetime.now().strftime('%I:%M:%S%p'))
+	refresh(update, tdelta)
 
 # Upload
 @app.route('/upload', methods = ['GET', 'POST'])
@@ -44,17 +51,37 @@ def upload():
 # Print info to console
 def printGrid():
 	global tdelta, x, y, grid
-	for i in range(x):
-		for j in range(y):
-			print(grid[i][j], end=' ')
-		print()
-	refresh(printGrid, tdelta)
+	# for i in range(x):
+	# 	for j in range(y):
+	# 		print(grid[i][j], end=' ')
+	# 	print()
+	# for i in range(x):
+	# 	print('-------------------------------')
+	# 	print('Volume: ' + str(grid[i].vol) + ', Direction: ' + str(grid[i].dir) + ', Time: ' + str(grid[i].time))
+	# 	print('-------------------------------')
+	data = exact(grid)
+	print('-------------------------------')
+	print('Volume: ' + str(data.vol) + ', Direction: ' + str(data.dir) + ', Time: ' + str(data.time))
+	print('-------------------------------')
+	# refresh(printGrid, tdelta)
+
+# # Load index.html
+# @app.route('/index', methods = ['GET'])
+# def home():
+# 	global grid
+# 	refresh(home, tdelta)
+# 	return render_template('index.html', sound=exact(grid))
 
 # Home
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+	global grid
+	update()
 	printGrid()
-	return render_template('index.html')
+	display = exact(grid)
+	display.vol = round(display.vol, 2)
+	display.dir = round(display.dir, 2)
+	return render_template('index.html', sound=display)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=5000)
--- server/triangulate.py
+++ server/triangulate.py
@@ -1,2 +1,22 @@
+import random
+from random import random, randint
+from app import Sound
+
+def exact(grid): # takes in 3 sound objects, computes angle relative to s1 and volume of sound source (CCW angle is negative, CW is positive)
+	s1 = grid[0]
+	s2 = grid[1]
+	s3 = grid[2]
+	angle = ((s1.dir + s2.dir + s3.dir + 360) % 360) / 3
+	vol = (s1.vol + s2.vol + s3.vol) / 3
+	return Sound(angle, vol, s1.time)
+
+def gen_random(time):
+	angle = random() * 360
+	volume = random() * randint(10, 37)
+	s1 = Sound(volume + randint(-5, 8), angle + randint(-3, 3), time)
+	s2 = Sound(volume + randint(-17, 25), angle + randint(-7, 13), time)
+	s3 = Sound(volume + randint(-2, 10), angle + randint(-1, 32), time)
+	return [s1, s2, s3]
+
 def triangulate():
 	return 0;
