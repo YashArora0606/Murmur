b02e292cd8c07a44f744abbe1ffb2f4b968a25c9
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2019-10-31 01:49:40 -0400

    Fix bugs

--- pi/microphone.py
+++ pi/microphone.py
@@ -6,19 +6,24 @@ import wave
 """
 Microphone class to provide an interface for recording sound
 """
-class Microphone:
-    def __init__(self):
+class MicrophoneInterface:
+    def __init__(self, indexes=[]):
         self.p = pyaudio.PyAudio()
-        self.device_count = p.get_device_count()
-        self.devices = [p.get_device_info_by_index(i) for i in range(self.device_count)]
+        self.device_count = self.p.get_device_count()
+        self.devices = [self.p.get_device_info_by_index(i) for i in range(self.device_count)]
         self.streams = []
         self.frames = []
-    
+    """
+    Shows devices that are recognized currently
+    """
     def print_devices(self):
         for device in self.devices:
             print(device.get('name'))
-            
-    def start_stream(self):
+    
+    """
+    Starts recording on all audio devices 
+    """
+    def start_streams(self):
         for i in range(len(config.DEV_INDEXES)):
             self.streams.append(
                     self.p.open(
@@ -29,26 +34,36 @@ class Microphone:
                     input = True,
                     frames_per_buffer=config.CHUNK)
                 )
-        
+    
     def close_streams(self):
         for stream in self.streams:
             stream.stop_stream()
             stream.close()
-    
+    """
+    Reads data in stream 
+    """
     def read_stream(self):
         for i in range(len(self.streams)):
             frames[i] = []
             for i in range((config.MIC_RATE/config.CHUNK)*config.RECORD_TIME):
                 frames[i].append(self.streams[i].read(config.CHUNK))
-
+    """
+    Creates wav files
+    """
     def write_wav(self):
         for frame in self.frames:
-            wavefile = wave.open(str(time.time()) + config.OUTPUT_FILENAME,'wb')
+            wavefile = wave.open("./output/" + str(time.time()) + config.OUTPUT_FILENAME,'wb')
             wavefile.setnchannels(config.CHANNELS)
             wavefile.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
             wavefile.setframerate(config.MIC_RATE)
             wavefile.writeframes(b''.join(frames))
             wavefile.close()
 
-        
-        
+def main():
+    m = MicrophoneInterface()
+    m.print_devices()
+    print("done")
+    
+if __name__ == '__main__':
+    main()
+   
