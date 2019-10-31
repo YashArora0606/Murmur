8be99bc7fcddf72abab9b3cdeb133da46409e942
Yash Arora <yash_arora6@hotmail.com>
2019-10-31 15:21:42 -0400

    Update server/readAudio.py

--- server/readAudio.py
+++ server/readAudio.py
@@ -1,3 +1,5 @@
+#
+
 import pyaudio
 import struct
 import numpy as np
@@ -31,19 +33,25 @@ stream = p.open(
     frames_per_buffer = CHUNK
 )
 
+
+
 plt.ion()
+
 fig, ax = plt.subplots()
 
 x = np.arange(0, CHUNK)
 data = stream.read(CHUNK)
 data_int16 = struct.unpack(str(CHUNK) + 'h', data)
 line, = ax.plot(x, data_int16)
-#ax.set_xlim([xmin, xmax])
+# ax.set_xlim([xmin, xmax])
 ax.set_ylim([-2**15, (2**15)-1])
+
 
 while True:
     data = struct.unpack(str(CHUNK) + 'h', stream.read(CHUNK))
-    print(data)
-    # line.set_ydata(data)
-    # fig.canvas.draw()
-    # fig.canvas.flush_events()
+    # print(data)
+    # plt.plot(data)
+    plt.pause(0.0001)
+    line.set_ydata(data)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
