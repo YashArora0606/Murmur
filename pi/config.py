999d61e97af9bda28a96709e0a4033ab86ca2c5a
tawnyzhao <tawnyzhao@gmail.com>
2019-11-02 00:26:32 -0400

    Update config files to include the correct mics

--- pi/config.py
+++ pi/config.py
@@ -7,8 +7,8 @@ UPLOAD_URL = ''
 DEVICE = 'pi'
 MIC_RATE = 44100
 CHUNK = 4096 #Number of samples per buffer
-CHANNELS = 1
+CHANNELS = 1L
 RECORD_TIME = 10 #Length of each recording in seconds
-DEV_INDEXES = [1] #TODO: Find index from pyaudio
+DEV_INDEXES = [2] #TODO: Find index from pyaudio
 OUTPUT_FILENAME = 'output.wav'
 
