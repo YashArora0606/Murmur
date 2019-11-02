a5f8b70a224f7d0d9f4dfa0196f67c8bdfca113f
tawnyzhao <tawnyzhao@gmail.com>
2019-11-02 01:04:10 -0400

    Fix wav file upload

--- pi/network.py
+++ pi/network.py
@@ -5,7 +5,7 @@ from microphone import MicrophoneInterface
 import config
 import requests
 PATH = os.getcwd()
-FILE_SUFFIX = re.compile("*.wav$")
+FILE_SUFFIX = r".*\.wav"
 
 file_list = list(filter(lambda file: re.match(FILE_SUFFIX, file), os.listdir('./output')))
 
@@ -16,10 +16,17 @@ def upload_all(clear_after_upload = False):
     global file_list
     for file_name in file_list:
         with open(PATH+"/output/"+file_name, 'rb') as data:
+            print("Sending: %s to %s", file_name, config.UPLOAD_URL)
             headers = {'content-type': 'audio/wav'}
             r = requests.post(config.UPLOAD_URL, data=data, headers=headers)
-            print(r)
-            print(r.text)
-        if clear_after_upload and os.path.exists(PATH+"/output/"+file_name):
-            os.remove(PATH+"/output/"+file_name)
+            if clear_after_upload and os.path.exists(PATH+"/output/"+file_name):
+                os.remove(PATH+"/output/"+file_name)
+
+def main():
+    update_file_list()
+    upload_all()
+
+if __name__ == "__main__":
+    main()
+
 
