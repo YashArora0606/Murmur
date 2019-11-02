704d633549c65f3c4421258a2f6ee85b04928f80
tawnyzhao <tawnyzhao@gmail.com>
2019-11-02 03:01:58 -0400

    Bugfix post request headers

--- pi/network.py
+++ pi/network.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import re 
+import re
 from microphone import MicrophoneInterface
 import config
 import requests
@@ -10,15 +10,17 @@ FILE_SUFFIX = r".*\.wav"
 file_list = list(filter(lambda file: re.match(FILE_SUFFIX, file), os.listdir('./output')))
 
 def update_file_list():
+    global file_list
     file_list = list(filter(lambda file: re.match(FILE_SUFFIX, file), os.listdir('./output')))
 
 def upload_all(clear_after_upload = False):
     global file_list
     for file_name in file_list:
         with open(PATH+"/output/"+file_name, 'rb') as data:
-            print("Sending: %s to %s", file_name, config.UPLOAD_URL)
-            headers = {'content-type': 'audio/wav'}
-            r = requests.post(config.UPLOAD_URL, data=data, headers=headers)
+            print("Sending", PATH+"/output/"+file_name)
+            file = {'file':data}
+            r = requests.post(config.UPLOAD_URL, files=file)
+            print(r.status_code)
             if clear_after_upload and os.path.exists(PATH+"/output/"+file_name):
                 os.remove(PATH+"/output/"+file_name)
 
